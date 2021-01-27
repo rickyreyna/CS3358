675b2da76f6320172d60d102b3337a8d0f9723d2
Ricardo Reyna <60950368+rickyreyna@users.noreply.github.com>
2021-01-26 21:54:45 -0600

    Add files via upload

---
+++ Assignments/Assignment 6/Assignment 6B/cmake-build-debug/CMakeFiles/Assignment_6B.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment_6B.dir/Assign06P1B.cpp.obj"
+  "CMakeFiles/Assignment_6B.dir/llcpImp.cpp.obj"
+  "Assignment_6B.pdb"
+  "Assignment_6B.exe"
+  "Assignment_6B.exe.manifest"
+  "libAssignment_6B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_6B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
