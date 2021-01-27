d0c1a06a1a43676d8b89201fd9cd754ca3af8264
Ricardo Reyna <60950368+rickyreyna@users.noreply.github.com>
2021-01-26 21:59:36 -0600

    Add files via upload

---
+++ Assignments/Assignment 8/cmake-build-debug/CMakeFiles/Assignment_8.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment_8.dir/Assign08.cpp.obj"
+  "CMakeFiles/Assignment_8.dir/HashTable.cpp.obj"
+  "Assignment_8.pdb"
+  "Assignment_8.exe"
+  "Assignment_8.exe.manifest"
+  "libAssignment_8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
